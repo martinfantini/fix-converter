9df8f2f64c8ff7835da8d8a18bd75c852dd8f8ee
Roberto Martin Fantini <martin.fantini@gmail.com>
2025-08-06 07:33:15 +0200

    Add init files

---
+++ app/generation/__init__.py
@@ -0,0 +1,2 @@
+# Copyright (C) 2025 R Martin Fantini <martin.fantini@gmail.com>
+# This file may be distributed under the terms of the GNU GPLv3 license
---
+++ app/generation/cppng/__init__.py
@@ -0,0 +1,4 @@
+# Copyright (C) 2025 R Martin Fantini <martin.fantini@gmail.com>
+# This file may be distributed under the terms of the GNU GPLv3 license
+
+from .generator import Generator
